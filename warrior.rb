7971da73a434acdc3b8d96e003558d3fe21b4d5a
vrto <michal.vrtiak+github@gmail.com>
2013-12-23 20:45:52 +0100

    level 7

---
+++ warrior.rb
@@ -0,0 +1,47 @@
+class Player
+
+  def initialize
+    @direction = :backward
+  end
+
+  def play_turn(warrior)
+    handle_direction(warrior)
+    space = warrior.feel(@direction)
+    if (should_turn_around?(warrior))
+      then warrior.pivot!
+    elsif (should_run_away?(warrior))
+      then warrior.walk!(:backward)
+    elsif (safe_to_rest?(warrior, space))
+      then warrior.rest!
+    elsif (space.captive?)
+      then warrior.rescue!(@direction)
+    elsif (space.enemy?) 
+      then warrior.attack!(@direction)
+    else
+      warrior.walk!(@direction)
+    end
+
+    @last_health = warrior.health
+  end
+
+  def safe_to_rest?(warrior, space)
+    warrior.health < 20 && space.empty? && warrior.health >= @last_health
+  end
+
+  def handle_direction(warrior)
+    if (warrior.feel(@direction).wall?) 
+      then @direction = :forward
+    end
+  end
+
+  def should_run_away?(warrior)
+    # nil when start of the game
+    @last_health != nil && warrior.health < 10 && warrior.health < @last_health
+  end
+
+  def should_turn_around?(warrior)
+    @last_health == nil && warrior.feel(:forward).wall?
+  end
+
+end
+  
